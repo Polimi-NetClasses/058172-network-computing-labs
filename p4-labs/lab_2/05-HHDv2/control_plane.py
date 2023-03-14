965ef7d5fe28dc9993c484ee57341e977e7c8b4b
Sebastiano Miano <mianosebastiano@gmail.com>
2023-03-14 14:48:16 +0100

    Fix network topo file solution #1

--- p4-labs/lab_2/05-HHDv2/control_plane.py
+++ p4-labs/lab_2/05-HHDv2/control_plane.py
@@ -17,7 +17,7 @@ controller = controllers['s1']
 controller.table_clear('ipv4_lpm')
 
 controller.table_add('ipv4_lpm', 'ipv4_forward', ['10.0.1.1/32'], ['00:00:0a:00:01:01', '1'])
-controller.table_add('ipv4_lpm', 'ipv4_forward', ['10.0.0.2/32'], ['00:00:00:02:01:00', '2'])
+controller.table_add('ipv4_lpm', 'ipv4_forward', ['10.0.2.2/32'], ['00:00:00:02:01:00', '2'])
 
 controller.set_default_action('ipv4_lpm', 'drop')
 
